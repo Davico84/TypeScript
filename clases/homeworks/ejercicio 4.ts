d23781fdd350cf8020ff01dc846ddd94d76faa00
Davico84 <davicova84@gmail.com>
2023-04-23 14:16:01 -0500

    hm 4

--- clases/homeworks/ejercicio 4.ts
+++ clases/homeworks/ejercicio 4.ts
@@ -5,20 +5,45 @@ let mati = {
 }
 
 //tipado explicito?
-
 interface Persona {
     nombre: string;
     edad: number
 }
+interface Carro {
+    id: number;
+    Nombre: string;
+    Marca:string;
+    Año:number;
+}
 
+let car: Carro={
+    id:1,
+    Nombre:"Camioneta SUV",
+    Marca: "Mahindra",
+    Año: 2012
+}
+
+
+interface Suv extends Carro{
+    Modelo: string;
+}
 let franco: Persona = {
     nombre: "Franco",
     edad: 27
 }
+let xuv500: Suv={
+    id:1,
+    Nombre:"Camioneta SUV",
+    Marca: "Mahindra",
+    Año: 2012,
+    Modelo: "XUV500 w6"
+}
+console.log(xuv500)
 
 interface Estudiante extends Persona {
     esActivo: boolean
 }
+
 
 let diego: Estudiante = {
     nombre: "Diego",
